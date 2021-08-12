62f3fc382cd653c4076ae6465010de7b1df3c676
yigitopan <81679099+yigitopan@users.noreply.github.com>
2021-08-12 18:02:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ curl_setopt_array($curl, array(
 $response = curl_exec($curl);
 $err = curl_error($curl);
 $decoded = json_decode($response, true);
-echo $decoded['customer']['email'];
+print_r($decoded['customer']['email']);
 curl_close($curl);
 
 if ($err) {
