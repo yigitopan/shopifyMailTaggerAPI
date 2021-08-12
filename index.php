2140bdc3ab2a83ae8b991976469c84b145c97d5b
yigitopan <yigitopan@hotmail.com>
2021-08-12 17:02:36 +0300

    deneme echo

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ curl_setopt_array($curl, array(
 
 $response = curl_exec($curl);
 $err = curl_error($curl);
-$decoded = json_decode($response);
+$decoded = json_decode($response, true);
 echo $decoded['email'];
 curl_close($curl);
 
