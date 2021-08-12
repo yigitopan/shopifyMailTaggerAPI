8d6fc9f853dd2811dea0be0d3c28fec6553b1b5f
yigitopan <yigitopan@hotmail.com>
2021-08-12 20:50:43 +0300

    deneme echo

--- index.php
+++ index.php
@@ -1,59 +1,9 @@
 <?php
-/* bi dk
-include_once("shopify.php");
-
-$shopify = new Shopify();
-
-$shopify->set_url("achteck.myshopify.com");
-$shopify->set_apikey("9d818fbe82d6ab7751f028ff2966ab20");
-$shopify->set_password("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");
-
-$customers = $shopify->rest_api('/admin/api/2021-07/customers.json', array(), 'GET');
-$customers = json_decode($customers['body'], true);
-
-echo print_r($customers);
-
-*/
-
-/* asagidaki GET, sadece bu class'ı kullanarak calısıyor
-include_once("shopify.php");
-
-$shopify = new Shopify();
-
-$store = "achteck.myshopify.com";
-$schlussel = "9d818fbe82d6ab7751f028ff2966ab20";
-$pw = "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";
-$url = 'https://' . $schlussel . ':' . $pw . '@' . $store . '/admin/api/2021-07/customers.json';
-$ch = curl_init();
-
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-
-
-$headers = array();
-$headers[] = 'Content-Type: application/json';
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-$result = curl_exec($ch);
-$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
-}
-
-$decoded = json_decode($result);
-print_r($decoded);
-curl_close($ch);
-
-*/
-
-// get start
-
+/* büyük resim
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://9d818fbe82d6ab7751f028ff2966ab20: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@achteck.myshopify.com/admin/api/2021-07/customers/5442193359048.json",
+    CURLOPT_URL => "https://9d818fbe82d6ab7751f028ff2966ab20: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@achteck.myshopify.com/admin/api/2021-07/customers.json",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => "",
     CURLOPT_MAXREDIRS => 10,
@@ -71,7 +21,39 @@ curl_setopt_array($curl, array(
 $response = curl_exec($curl);
 $err = curl_error($curl);
 $decoded = json_decode($response, true);
-$mail = $decoded['customer']['email'];
+
+curl_close($curl);
+
+
+if ($err) {
+    echo "cURL Error is  #:" . $err;
+}
+      büyük resim */
+
+// +1. satırdan +37. satıra, burada mail domaini alınıyor.
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://9d818fbe82d6ab7751f028ff2966ab20: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@achteck.myshopify.com/admin/api/2021-07/customers.json",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+        "authorization: Basic OWQ4MThmYmU4MmQ2YWI3NzUxZjAyOGZmMjk2NmFiMjA6c2hwcGFfZjEzZDYyNmQxZWYyNDA4MzYxNDBhMDhkNTM5OWQwMjA=",
+        "cache-control: no-cache",
+        "content-type: application/json",
+        "postman-token: af2f7c61-036e-5ac4-4b9f-9d328a1117c9"
+    ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+$decoded = json_decode($response, true);
+$mail = $decoded['customers'][0]['email'];
+
 
 
 curl_close($curl);
@@ -86,6 +68,7 @@ echo "<br>";
 if ($err) {
     echo "cURL Error is  #:" . $err;
 }
+
 
 // put start
 
