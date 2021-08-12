677ba7e22508d4540489584b8c4b14a925be6c96
yigitopan <yigitopan@hotmail.com>
2021-08-12 09:29:23 +0300

    deneme

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
+<?php
 
+include_once("shopify.php");
+
+$shopify = new Shopify();
+
+$shopify->set_url("achteck.myshopify.com");
+$shopify->set_apikey("9d818fbe82d6ab7751f028ff2966ab20");
+$shopify->set_password("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");
+
+$customers = $shopify->rest_api('/admin/api/2021-07/customers.json', array(), 'GET');
+$customers = json_decode($customers['body'], true);
+
+echo print_r($customers);
+?>
--- shopify.php
+++ shopify.php
@@ -17,6 +17,53 @@ class Shopify {
         $this->shop_url = $url;
     }
 
+    public function rest_api($api_endpoint, $query = array(), $method = 'GET'){
+        $url = 'https://'.$this->api_key.':'.$this->password.'@'.$this->shop_url.$api_endpoint;
+
+        if (in_array($method, array('GET', 'DELETE')) && is_null($query)) {
+            $url = $url.'?'.http_build_query($query);
+        }
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HEADER,true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION,true);
+        curl_setopt($ch, CURLOPT_MAXREDIRS,3);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,false);
+        curl_setopt($ch, CURLOPT_TIMEOUT,30);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT,true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+
+        if( $method != 'GET' && in_array($method, array('POST', 'PUT'))){
+            if(is_array($query)) $query = http_build_query($query);
+            curl_setopt($ch, CURLOPT_POSTFIELDS,$query);
+        }
+
+        $response = curl_exec($ch);
+        $error = curl_errno($ch);
+        $errmsg = curl_error($ch);
+
+        curl_close($ch);
+
+        if($error){
+            return $errmsg;
+        }
+        else {
+            $response = preg_split("/\r\n\r\n|\n\n|\r\r/", $response, 2);
+            $headers = array();
+            $content = explode("\n", $response[0]);
+            $headers['status'] = $content[0];
+
+            array_shift($content);
+
+            foreach ($content as $c) {
+                $data = explode(":", $c);
+                $headers[trim($data[0])] = $data [1];
+            }
+
+            return array('headers' => $headers, 'body' => $response[1]);
+        }
+    }
+
 }
 
 ?>
