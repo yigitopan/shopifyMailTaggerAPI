577f98ca007c5898a45726b83ff2c7540cf2da05
yigitopan <81679099+yigitopan@users.noreply.github.com>
2021-08-14 15:28:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 // +1. satırdan +37. satıra, burada mail domaini alınıyor.
+
+define('SHOPIFY_APP_SECRET', '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');
+
+function verify_webhook($data, $hmac_header)
+{
+  $calculated_hmac = base64_encode(hash_hmac('sha256', $data, SHOPIFY_APP_SECRET, true));
+  return hash_equals($hmac_header, $calculated_hmac);
+}
+
+
+$hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
+$data = file_get_contents('php://input');
+$verified = verify_webhook($data, $hmac_header);
+error_log('Webhook verified: '.var_export($verified, true)); //check error.log to see the result
+
+// verify bitis
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
